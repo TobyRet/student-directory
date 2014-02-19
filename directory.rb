160d9de7ce8ddb22542d32f9625202fbafb8b228
Toby Retallick <toby.retallick@gmail.com>
2014-02-19 13:28:49 +0000

    Created method to allow user to input students via terminal

--- directory.rb
+++ directory.rb
@@ -1,4 +1,6 @@
 # students array
+
+=begin
 students = [
 	{:name => "Mario Gintili", :cohort => :February},
 	{:name => "Mikhail Dubov", :cohort => :February},
@@ -24,7 +26,20 @@ students = [
  	{:name => "Anna Yanova", :cohort => :February},
  	{:name => "Andrew Cumine", :cohort => :February}
 ]
+=end
 
+def input_students
+	puts "Please enter the name of the students."
+	puts "To finish, hit return twice."
+	students = []
+	name = gets.chomp
+	while !name.empty? do 
+		students << {:name => name, :cohort => :February}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	end
+	students
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -41,6 +56,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students."
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
