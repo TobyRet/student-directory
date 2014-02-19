f26013f9d366d27b3b0aeadac8ca4f14762dfddb
Toby Retallick <toby.retallick@gmail.com>
2014-02-19 18:32:29 +0000

    added a user input for 'cohort'

--- challenges.rb
+++ challenges.rb
@@ -1,6 +1,6 @@
 # students array
 
-
+=begin
 students = [
 	{:name => "Mario Gintili", :cohort => :February},
 	{:name => "Mikhail Dubov", :cohort => :February},
@@ -26,18 +26,19 @@ students = [
  	{:name => "Anna Yanova", :cohort => :February},
  	{:name => "Andrew Cumine", :cohort => :February}
 ]
-
+=end
 
 
 def input_students
-	puts "Please enter the name of the students."
-	puts "To finish, hit return twice."
+	puts "Please enter the name of the student."
 	students = []
 	name = gets.chomp
-	while !name.empty? do 
-		students << {:name => name, :cohort => :February}
+	puts "Please enter the cohort for #{name}"
+	cohort = gets.chomp
+	while !name.empty? && !cohort.empty? do 
+		students << {:name => name, :cohort => cohort}
+		puts "you have added #{name} to the #{cohort} cohort"
 		puts "Now we have #{students.length} students"
-		name = gets.chomp
 	end
 	students
 end
@@ -47,18 +48,9 @@ def print_header
 	puts "-------------------------------------------"
 end
 
-def print(students)
-	
+def print(students)	
 	students.each_with_index do |student|
-		# name begins with a
-		#if student[:name].chr.downcase == 'a'
-			#puts "#{students.index(student) + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
-		#end
-
-		# name shorter than twelve characters
-		if student[:name].length < 12
-			puts "#{students.index(student) + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
-		end
+		puts "#{students.index(student) + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
 	end
 end
 
@@ -66,7 +58,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students."
 end
 
-#students = input_students
+students = input_students
 print_header
 print(students)
 print_footer(students)
